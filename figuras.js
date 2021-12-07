79740a2c2699cdc9dd5a881810501bcd020f76c5
raul_cardenas16 <raulcarfer@gmail.com>
2021-12-07 00:21:46 -0500

    Encapsulando codigo en funciones

--- figuras.js
+++ figuras.js
@@ -1,4 +1,5 @@
-//Código del cuadrado
+ //Código del cuadrado
+/*
 console.group("Cuadrados");
 const ladoCuadrado = 5;
 console.log("Los lados del cuadrado mide: " + ladoCuadrado + "cm");
@@ -29,7 +30,7 @@ const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
 console.log("El área del triangulo es: " + areaTriangulo + "cm2");
 console.groupEnd();
 //Codigo del circulo
-console.group("Triangulos");
+console.group("Circulo");
 const radioCirculo = 4;
 const diametroCirculo = radioCirculo * 2;
 const PI = Math.PI;
@@ -40,4 +41,46 @@ console.log("El diametro del circulo es: " + diametroCirculo + "cm");
 console.log("PI es: " + PI);
 console.log("El perimetro del circulo es: " + perimetroCirculo + "cm");
 console.log("El área del circulo es: " + areaCirculo + "cm2");
+console.groupEnd();
+*/
+
+//Funciones
+
+console.group("Cuadrados");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+console.groupEnd();
+
+//Código del triángulo
+console.group("Triangulos");
+
+function perimetroTriangulo(lado1,lado2,base) {
+    return lado1 + lado2 + base;
+}
+function areaTriangulo(base,altura) {
+    return (base * altura) / 2;
+}
+
+console.groupEnd();
+
+//Codigo del circulo
+console.group("Circulo");
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+const PI = Math.PI;
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+
 console.groupEnd();
