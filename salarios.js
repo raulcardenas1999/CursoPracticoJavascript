f6ac63e11a952c971f15ba3ac27e0ae416a348f5
raul_cardenas16 <raulcarfer@gmail.com>
2021-12-09 01:09:34 -0500

    Calculando la mediana de los salarios

---
+++ salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 4500
+});
+colombia.push({
+    name: "Pedro",
+    salary: 1900
+});
+colombia.push({
+    name: "Juan",
+    salary: 1800
+});
+colombia.push({
+    name: "Camila",
+    salary: 1700
+});
+colombia.push({
+    name: "Ernesto",
+    salary: 1600
+});
+colombia.push({
+    name: "Eduardo",
+    salary: 1500
+});
+colombia.push({
+    name: "Luis",
+    salary: 1400
+});
+colombia.push({
+    name: "Sebastian",
+    salary: 1300
+});
+colombia.push({
+    name: "Jhon",
+    salary: 1200
+});
+colombia.push({
+    name: "Yuli",
+    salary: 1100
+});
+colombia.push({
+    name: "Andrea",
+    salary: 4000
+});
+colombia.push({
+    name: "Jose",
+    salary: 9000
+});
+colombia.push({
+    name: "Camilo",
+    salary: 8000
+});
+colombia.push({
+    name: "Fabian",
+    salary: 7000
+});
+colombia.push({
+    name: "Valentina",
+    salary: 6000
+});
+colombia.push({
+    name: "Gonzo",
+    salary: 5000
+});
+colombia.push({
+    name: "Jorge",
+    salary: 400
+});
+colombia.push({
+    name: "Rodolfo",
+    salary: 3000
+});
+colombia.push({
+    name: "Cristian",
+    salary: 2000
+});
+colombia.push({
+    name: "Andres",
+    salary: 1000
+});
